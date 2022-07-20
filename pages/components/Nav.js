7b679c5749b5165118d63a664060c3e82fc0891e
Michael Mathias <mickmath86@gmail.com>
2022-07-19 20:47:39 -0700

    about mobile link

--- pages/components/Nav.js
+++ pages/components/Nav.js
@@ -321,9 +321,12 @@ export default function Nav() {
             <div className="py-6 px-5">
               
               <div className="grid grid-cols-2 gap-4">
-                <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
-                  About
-                </a>
+                <Link href="#" >
+                  <div className="text-base font-medium text-gray-900 hover:text-gray-700">
+                      About
+                  </div>
+                  
+                </Link>
 
                 <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
                   Docs
