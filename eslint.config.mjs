d0c662869c59d29557d052b55ab364e592808c20
Michael Mathias <mickmath86@gmail.com>
2025-07-21 22:26:49 -0700

    turn off all eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,7 @@ const eslintConfig = [
   {
     // files: ["**/*.ts", "**/*.tsx"],
     rules: {
-      "@typescript-eslint/no-unused-expressions": "off",
+      // "@typescript-eslint/no-unused-expressions": "off",
     },
   },
 ];
