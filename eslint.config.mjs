6d3db37c0d36a9fc300d1da6246f9b101c0dc2b3
Michael Mathias <mickmath86@gmail.com>
2025-07-21 22:28:47 -0700

    retry eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,12 +11,35 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Disable all rules
   {
-    // files: ["**/*.ts", "**/*.tsx"],
-    rules: {
-      // "@typescript-eslint/no-unused-expressions": "off",
-    },
+    files: ["**/*.{js,ts,jsx,tsx}"],
+    rules: {},
   },
 ];
 
 export default eslintConfig;
+
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+//   {
+//     // files: ["**/*.ts", "**/*.tsx"],
+//     rules: {
+//       "@typescript-eslint/no-unused-expressions": "off",
+//     },
+//   },
+// ];
+
+// export default eslintConfig;
