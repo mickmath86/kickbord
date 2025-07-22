38a14c69581209a557521db1451442e93470330c
Michael Mathias <mickmath86@gmail.com>
2025-07-21 22:57:58 -0700

    EOD updates

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,13 +5,13 @@ import { Link } from './link'
 const variants = {
   primary: clsx(
     'inline-flex items-center justify-center px-4 py-[calc(--spacing(2)-1px)]',
-    'rounded-full border border-transparent bg-gray-950 shadow-md',
+    'rounded-sm border border-transparent bg-gray-950 shadow-md',
     'text-base font-medium whitespace-nowrap text-white',
     'data-disabled:bg-gray-950 data-disabled:opacity-40 data-hover:bg-gray-800',
   ),
   secondary: clsx(
     'relative inline-flex items-center justify-center px-4 py-[calc(--spacing(2)-1px)]',
-    'rounded-full border border-transparent bg-white/15 shadow-md ring-1 ring-[#D15052]/15',
+    'rounded-sm border border-transparent bg-white/15 shadow-md ring-1 ring-[#D15052]/15',
     'after:absolute after:inset-0 after:rounded-full after:shadow-[inset_0_0_2px_1px_#ffffff4d]',
     'text-base font-medium whitespace-nowrap text-gray-950',
     'data-disabled:bg-white/15 data-disabled:opacity-40 data-hover:bg-white/20',
